f54a4793c643d2b449ca3cd3f2226edfb2786bf3
HenryLiu2015 <liu46829@126.com>
2020-07-06 11:23:28 +0800

    add logout register

--- app/models.py
+++ app/models.py
@@ -1,6 +1,9 @@
-from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from app import db, login
 from datetime import datetime
 from flask_login import UserMixin
+
 
 
 class User(UserMixin, db.Model):
@@ -13,6 +16,12 @@ class User(UserMixin, db.Model):
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -22,3 +31,10 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
